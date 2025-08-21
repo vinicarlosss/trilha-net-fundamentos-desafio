b39a32a95a551a0381a1de0c4fd3d845ab092702
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2025-08-20 22:58:23 -0300

    feat: adding method RemoverVeiculo

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -36,26 +36,27 @@ namespace DesafioFundamentos.Models
 
         public void RemoverVeiculo()
         {
-            Console.WriteLine("Digite a placa do veículo para remover:");
-
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
-
+            Console.WriteLine("Digite a placa do veículo para remover: (Modelo antigo: ABC1234 || Modelo Mercosul: BRA2E19)");
+            string placa = Console.ReadLine();
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
-                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
-
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
+                bool horaValida = false;
                 int horas = 0;
-                decimal valorTotal = 0;
-
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
-
+                while (!horaValida)
+                {
+                    Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                    if (int.TryParse(Console.ReadLine(), out horas))
+                    {
+                        horaValida = !horaValida;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Valor inválido!");
+                    }
+                }
+                decimal valorTotal = precoInicial + precoPorHora * horas;
+                veiculos.Remove(placa);
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
             else
